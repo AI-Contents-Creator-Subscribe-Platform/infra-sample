0f4c2792d9c29cfbcf1a7ce69c214349d34aaeeb
sheep1500 <1500sheep@gmail.com>
2024-09-09 11:46:52 +0900

    project init

---
+++ src/test/java/org/sheep1500/toyadvertisementbackend/ToyAdvertisementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sheep1500.toyadvertisementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyAdvertisementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
